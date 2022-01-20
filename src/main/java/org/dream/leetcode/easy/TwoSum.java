11b89042af1fb50ff3d188889e8e3a796a4c1b74
george_chou <george_chou@gss.com.tw>
2022-01-21 05:37:15 +0800

    O(n) TwoSum

--- src/main/java/org/dream/leetcode/easy/TwoSum.java
+++ src/main/java/org/dream/leetcode/easy/TwoSum.java
@@ -1,17 +1,22 @@
 package org.dream.leetcode.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                int num1 = nums[i];
-                int num2 = nums[j];
-
-                if(num1 + num2 == target) {
-                    return new int[] {i, j};
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int currentIndex = 0; currentIndex < nums.length; currentIndex++) {
+            int num1 = nums[currentIndex];
+            int num2 = target - num1;
+            if(map.containsKey(num2)) {
+               Integer num2Index = map.get(num2); 
+               if(num2Index != currentIndex) {
+                   return new int[] { num2Index, currentIndex };
+               }
             }
+            map.put(nums[currentIndex], currentIndex);
         }
         return new int[0];   
     }
